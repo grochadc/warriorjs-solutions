d4c1a2c4934949dfd307e591f6277af92021c802
Gonzalo Rocha <grochadc@gmail.com>
2018-05-22 17:43:27 -0500

    Level 3 Solution

--- hectorescaton-beginner/Player.js
+++ hectorescaton-beginner/Player.js
@@ -2,7 +2,11 @@ class Player {
   playTurn(warrior) {
     // Cool code goes here.
     if(warrior.feel().isEmpty()){
+      if(warrior.health() < 20){
+        warrior.rest();
+      } else {
         warrior.walk();
+      }
     }
     else {
      warrior.attack();
