47253234d78f758f8cb18f78b705b0233538957c
Gonzalo Rocha <grochadc@gmail.com>
2018-05-22 18:42:27 -0500

    Level 6 Solution

--- hectorescaton-beginner/Player.js
+++ hectorescaton-beginner/Player.js
@@ -2,19 +2,31 @@ class Player {
   playTurn(warrior) {
     // Cool code goes here.
     let engaged = warrior.health() < this.lastTurnsHP;
+    this.direction = this.direction ? this.direction : 'backward';
 
-    if(warrior.feel().isEmpty()){
+    //NOTHING IN current DIRECTION
+    if(warrior.feel(this.direction).isEmpty()){
       if(!engaged && warrior.health() < 20){
         warrior.rest();
-      } else { //nothing in front
-        warrior.walk();
+      }
+      else if(!engaged && warrior.health() == 20){
+        warrior.walk(this.direction);
+      }
+      else if(engaged){
+        let newDir = warrior.health() > 15 ? 'forward' : 'backward';
+        warrior.walk(newDir);
       }
     }
     else { //something in front
-      if(warrior.feel().getUnit().isEnemy()){
-        warrior.attack();
+      if(warrior.feel(this.direction).getUnit() && warrior.feel(this.direction).getUnit().isEnemy()){
+        warrior.attack(this.direction);
       } else {
-        warrior.rescue();
+        if(warrior.feel(this.direction).getUnit() && warrior.feel(this.direction).getUnit().isBound()){
+            warrior.rescue(this.direction);
+        } else {
+          warrior.think('Changing direction');
+          this.direction = 'forward';
+        }
       }
    }
    this.lastTurnsHP = warrior.health();
