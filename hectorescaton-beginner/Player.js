1dade6d78021464c7c1972faa2cd0f166135c3ea
Gonzalo Rocha <grochadc@gmail.com>
2018-05-22 23:07:50 -0500

    Level 9 bad solution

--- hectorescaton-beginner/Player.js
+++ hectorescaton-beginner/Player.js
@@ -1,39 +1,13 @@
-class Player {
-  playTurn(warrior) {
-    // Cool code goes here.
-    let engaged = warrior.health() < this.lastTurnsHP;
-    if(warrior.look()[0].getUnit() && warrior.look()[0].getUnit().isEnemy()){
-      warrior.shoot();
-      return;
-    }
 
-    //NOTHING IN current DIRECTION
-    if(warrior.feel().isEmpty()){
-      if(!engaged && warrior.health() < 20){
-        warrior.rest();
-      }
-      else if(!engaged && warrior.health() == 20){
-        warrior.walk();
-      }
-      else if(engaged){
-        if(warrior.look()[1].getUnit() && warrior.look()[1].getUnit().isEnemy()){
-          warrior.shoot();
-          return;
-        }
-        let newDir = warrior.health() > 15 ? 'forward' : 'backward';
-        warrior.walk(newDir);
-      }
+class Player {
+
+  playTurn(warrior) {
+    if(warrior.feel('backward').isEmpty()){
+      warrior.walk('backward');
+    } else if(warrior.feel('backward').getUnit().isBound()){
+      warrior.rescue('backward');
+    } else {
+      warrior.attack('backward');
     }
-    else { //something in front
-      if(warrior.feel().getUnit() && warrior.feel().getUnit().isEnemy()){
-        warrior.attack();
-      } else if(warrior.feel().getUnit() && warrior.feel().getUnit().isBound()){
-          warrior.rescue();
-        } else {
-          warrior.think('Changing direction');
-          warrior.pivot();
-        }
-      }
-      this.lastTurnsHP = warrior.health();
-   }
+  }
 }
